735314d510705e3ebcef212117f0305b25841592
Molly :) <mollyflanagan4@outlook.com>
2023-07-17 13:37:53 +0100

    commit

--- BankAccount.cs
+++ BankAccount.cs
@@ -7,9 +7,16 @@ using System.Threading.Tasks;
 
     internal class BankAccount
 {
+    public static int newAccountNumber = 100000;
+
     public string CustomerName { get; set; }
     public int AccountNumber { get; set; }
     public float Balance { get; set; }
 
+    public BankAccount(string customerName) { 
 
+        CustomerName = customerName;
+        AccountNumber =
+        Balance = 0;
+    }
 }
